5127d771a4999f9ef8badab889c7342db94a689a
Brian Foster <bdfoster89@gmail.com>
2017-09-11 09:49:15 -0400

    Add Record class, add tests, add travis-ci config

---
+++ src/Record.ts
@@ -0,0 +1,447 @@
+import * as get from 'lodash.get';
+import * as pick from 'lodash.pick';
+import * as set from 'lodash.set';
+import * as unset from 'lodash.unset';
+import {EventEmitter} from 'nomatic-events';
+import {isNullOrUndefined} from "util";
+
+export type RecordOperation = 'add' | 'replace' | 'remove';
+export type RecordSaveFunction = (record: Record) => Promise<boolean | RecordData | Record>;
+export type RecordVirtualPropertyFunction = () => any;
+export interface RecordChange {
+    operation?: RecordOperation;
+    key: string;
+    old: any;
+    new: any;
+}
+
+export interface RecordData {
+    id?: string;
+    rev?: string;
+    [key: string]: any;
+}
+
+export interface RecordOptions {
+    maxListeners?: number;
+
+    /**
+     * Function to handle syncing changes of the Record instance to the storage device.
+     */
+    save?: RecordSaveFunction;
+
+    /**
+     * Object to describe virtual properties.
+     */
+    virtuals?: RecordVirtualProperties;
+}
+
+export interface RecordVirtualProperties {
+    [key: string]: RecordVirtualPropertyOptions | RecordVirtualPropertyFunction;
+}
+
+export interface RecordVirtualPropertyOptions {
+    /**
+     * Function which returns the value of the virtual property.
+     * @returns {any}
+     */
+    get?: RecordVirtualPropertyFunction;
+
+    /**
+     * Function which sets the value of the virtual property. Can be used to set
+     * other properties within the Record instance.
+     * @param value
+     */
+    set?: (value: any) => void;
+
+    /**
+     * Should the output of `get` handler be saved to the storage device?
+     */
+    save?: boolean;
+
+    /**
+     * Should the output of `get` handler be included in the serialized Record?
+     */
+    serialize?: boolean;
+}
+
+
+/**
+ * A trackable object with configurable `save` handler. Changes to the object can be reverted incrementally
+ * in the reverse order the changes were made.
+ * @class Record
+ * @extends EventEmitter
+ */
+export class Record extends EventEmitter {
+    [key: string]: any;
+
+    private _changes: RecordChange[];
+    private _data: RecordData;
+    private _save: RecordSaveFunction;
+    private _virtuals: Map<string, RecordVirtualPropertyOptions>;
+
+    constructor (options: RecordOptions | Record, data?: RecordData) {
+        super(options.maxListeners || 0);
+        this._changes = [];
+        this._data = {};
+
+        if (options instanceof Record) {
+            this.init(options);
+        } else {
+            options = options || {};
+            this._save = options.save || null;
+            this.init(data, options.virtuals);
+        }
+
+        return this.proxy();
+    }
+
+    /**
+     * Return the changes since last `save()` or `init()`. By default, it returns all changes.
+     * @param {number} count Number of changes to return (in reverse chronological order).
+     * @returns {RecordChange[]}
+     */
+    public changes(count: number = null) {
+        if (count === null) count = this._changes.length;
+        const start = (this._changes.length) - count;
+        const end = start + count;
+        return this._changes.slice(start, end);
+    }
+
+    /**
+     * Set virtual property definitions.
+     * @param {RecordVirtualProperties} value
+     */
+    public set virtuals(value: RecordVirtualProperties) {
+        const ignoredKeys = ['set', 'get', 'save', 'serialize'];
+        this._virtuals = new Map();
+
+        function recurse(path) {
+            let target = get(value, path);
+            if (target instanceof Function) {
+                target = {
+                    get: target
+                };
+            }
+
+            if (!target.hasOwnProperty('save')) target.save = false;
+            if (!target.hasOwnProperty('serialize')) target.serialize = true;
+
+            this._virtuals.set(path, pick(target, ignoredKeys));
+
+            for (const key in target) {
+                if (target.hasOwnProperty(key)) {
+                    if (ignoredKeys.indexOf(key) === -1) {
+                        recurse.apply(this, [path + '.' + key]);
+                    }
+                }
+            }
+        }
+
+        for (const key in value) {
+            recurse.apply(this, [key]);
+        }
+    }
+
+    /**
+     * Recursive/nested proxy. This is normally returned from `constructor()` method.
+     *
+     * @param {any} path
+     * @returns {any}
+     */
+    private proxy(path = null) {
+        const self = this;
+
+        const traps = {
+            get(target, key) {
+                if (path) key = path + '[' + key.toString() + ']';
+                if (get(self, key)) {
+                    return get(self, key);
+                } else if (typeof self.get(key) === 'object' && self.get(key) !== null) {
+                    return self.proxy(key);
+                }
+                return self.get(key);
+            },
+            set(target, key, value) {
+                if (path) {
+                    if (!isNaN(key) ) {
+                        key = path + '[' + key.toString() + ']';
+                    } else {
+                        key = path + '.' + key;
+                    }
+                }
+                if (get(self, key)) {
+                    if (value === null) {
+                        self.unset(key);
+                    } else {
+                        set(self, key, value);
+                    }
+                } else {
+                    self.set(key, value);
+                }
+                return true;
+            },
+            deleteProperty(target, key) {
+                if (path) key = path + '[' + key + ']';
+
+                if (get(self, key)) {
+                    return false;
+                }
+                return self.unset(key);
+            }
+        };
+
+        if (path === null) {
+            return new Proxy(this, traps);
+        }
+
+        return new Proxy(get(this._data, path), traps);
+    }
+
+    /**
+     * Initialize the instance. Data passed here is assumed to be the ground truth with respect to changes. All previous
+     * changes are cleared on each call.
+     *
+     * @param {RecordData} data
+     * @param {RecordVirtualProperties} virtuals
+     */
+
+    public init(data: Record | RecordData, virtuals: RecordVirtualProperties = null) {
+        if (data instanceof Record) {
+            // Essentially clone the old Record instance
+            this._data = Object.assign({}, data._data);
+            this._changes = data._changes.slice();
+            this._virtuals = new Map(data._virtuals);
+            this._save = data._save;
+            return;
+        }
+
+        return this.commit(data, virtuals);
+    }
+
+    public commit(data: string | number | boolean | RecordData | Record, virtuals: RecordVirtualProperties = null) {
+        if (virtuals) {
+            this.virtuals = virtuals;
+        } else if (!this._virtuals) {
+            this._virtuals = new Map();
+        }
+
+        if (typeof data === 'object') {
+            if (data instanceof Record) {
+                this._data = data._data;
+            } else {
+                this._data = data;
+            }
+
+
+        } else {
+            if (data !== true) {
+                this.rev = data;
+            }
+        }
+
+        for (const entry of this._virtuals.entries()) {
+            if (entry[1].save) {
+                const value = get(this._data, entry[0]);
+                if (entry[1].set && !isNullOrUndefined(value)) {
+                    entry[1].set(value);
+                } else {
+                    set(this._data, entry[0], entry[1].get());
+                }
+            }
+        }
+
+        this._changes = [];
+    }
+
+    /**
+     * Convenience method for returning value at `key` in `_data`. `key` can be a string or Array describing the path
+     * under `_data`.
+     *
+     * @param key The path under `_data`.
+     * @returns The value at `key` under `_data`.
+     */
+    public get(key: (string | number)[] | string): any {
+        if (typeof key !== 'string') key = key.toString();
+        if (this._virtuals.has(key)) return this._virtuals.get(key).get.apply(this);
+
+        return get(this._data, key);
+    }
+
+    /**
+     * Set a value at `key` in `_data`. Anything set using this method will have a corresponding entry in `_changes`.
+     *
+     * @param key
+     * @param value
+     */
+    public set(key, value) {
+        if (typeof key !== 'string') key = key.toString();
+        let isVirtual = false;
+
+        if (this._virtuals.has(key)) {
+            const definition = this._virtuals.get(key);
+            if (definition.hasOwnProperty('set')) {
+                if (!definition.save) {
+                    definition.set(value);
+                    return true;
+                }
+                isVirtual = true;
+            } else {
+                return false;
+            }
+        }
+
+        const parentKey = key.split('.');
+        parentKey.pop();
+        const parent = this.get(parentKey);
+
+        if (parent instanceof Array && key.endsWith('length')) return;
+
+        const operation: RecordOperation = !!this.get(key) ? 'replace' : 'add';
+
+        let old = this.get(key);
+
+        if (isVirtual) {
+            this._virtuals.get(key).set(value);
+            value = this._virtuals.get(key).get.apply(this);
+        }
+
+        let newVal = value;
+
+        if (value instanceof Object) {
+            if (value instanceof Array) {
+                newVal = value.slice();
+            } else {
+                newVal = Object.assign({}, value);
+            }
+        }
+
+        if (operation === 'replace' && old instanceof Object) {
+            if (old instanceof Array) {
+                old = old.slice();
+            } else {
+                old = Object.assign({}, old);
+            }
+        }
+
+        const change = {
+            operation: operation,
+            key: key,
+            old: old,
+            new: newVal
+        };
+
+        set(this._data, key, value);
+
+        this._changes.push(change);
+    }
+
+    /**
+     * Essentially deletes a property at `key` under `_data`. Uses of this method will have a corresponding entry in
+     * `_changes` so long as a value exists. Virtual properties cannot be unset.
+     *
+     * @param key
+     * @returns {boolean} If true, unset was successful. If false, there's nothing set at `key` under `_data` or it is
+     * a virtual property.
+     */
+    public unset(key) {
+        if (!isNullOrUndefined(get(this._virtuals, key))) return false;
+        const operation: RecordOperation = 'remove';
+        let old = this.get(key);
+
+        if (old === undefined) return false;
+
+        if (old instanceof Object) {
+            old = Object.assign({}, old);
+        }
+
+        const change = {
+            operation: operation,
+            key: key,
+            old: old,
+            new: undefined
+        };
+
+        if (unset(this._data, key)) {
+            this._changes.push(change);
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Calls `_save()` (if it exists). All changes are considered committed if `_save` exists
+     * and returns successfully.
+     *
+     * @returns {Promise<void>}
+     */
+    public async save() {
+        if (this._save) {
+            try {
+                const result = await this._save(this);
+                this.commit(result);
+            } catch (e) {
+                throw e;
+            }
+        }
+    }
+
+    public async validate() {
+        if (this._validate) {
+            try {
+                await this._validate(this);
+            } catch(e) {
+                throw e;
+            }
+        }
+    }
+
+    /**
+     * Reverts changes to `_data` made by `set()` and `unset()` methods.
+     *
+     * @param {number} count The number of changes to revert (in reverse chronological order). By default, all changes
+     * are reverted.
+     */
+    public revert(count: number = this._changes.length) {
+        for (let i = 0; i < count; i++) {
+            const change = this._changes.pop();
+
+            switch(change.operation) {
+                case 'remove':
+                case 'replace':
+                    set(this._data, change.key, change.old);
+                    break;
+                case 'add':
+                    unset(this._data, change.key);
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Serialize the Record instance as an object. Called by `#toJSON()`.
+     * @returns {RecordData}
+     */
+    public serialize(): RecordData {
+        const result = Object.assign({}, this._data);
+
+        for (const entry of this._virtuals.entries()) {
+            const value = entry[1].get();
+            if (entry[1].serialize && !entry[1].save && !isNullOrUndefined(value)) {
+                set(result, entry[0], entry[1].get());
+            }
+
+            if (!entry[1].serialize) {
+                unset(result, entry[0]);
+            }
+        }
+
+        return result;
+    }
+
+    public toJSON(): RecordData {
+        return this.serialize();
+    }
+}
+
+export default Record;
---
+++ test/Record.test.ts
@@ -0,0 +1,225 @@
+import 'mocha';
+import {expect} from 'chai';
+import {Record, RecordData} from '../src/Record';
+
+describe('Record', () => {
+    let number = 100;
+    let saved = false;
+    let instance: Record;
+    let data = {
+        id: '12345',
+        rev: '0',
+        firstName: 'John',
+        lastName: 'Doe'
+    };
+
+    before(() => {
+        instance = new Record({
+            save(data: RecordData) {
+                return new Promise((resolve) => {
+                    this.emit('saved', data);
+                    resolve(true);
+                });
+            },
+            virtuals: {
+                hello() {
+                    return 'world'
+                },
+                number: {
+                    get() {
+                        return number;
+                    },
+                    set(value) {
+                        number = value;
+                    }
+                },
+                saved: {
+                    get() {
+                        return saved === true ? 'yes' : 'no';
+                    },
+                    set(value) {
+                        saved = value;
+                    },
+                    save: true,
+                    serialize: false
+                }
+            }
+        }, {});
+    });
+
+    it('should not throw on instantiation with no parameters defined', () => {
+        expect(new Record({})).to.not.throw;
+    });
+
+    it('should throw when trying to delete a property defined in proto', () => {
+        try {
+            delete instance['_data'];
+        } catch (e) {
+            if (e.name !== 'TypeError') throw e;
+        }
+    });
+
+    it('should retrieve value of virtual property', () => {
+        expect(instance.hello).to.equal('world');
+        expect(instance.number).to.equal(100);
+    });
+
+    describe('#init()', () => {
+        it('should properly initialize the instance', () => {
+            expect(instance.init(Object.assign({}, data))).to.not.throw;
+            expect(instance['_data']).to.deep.equal(Object.assign({}, data, {
+                saved: 'no'
+            }));
+        });
+
+        it('should clone if an existing Record instance is passed as `data`', () => {
+            const clone = new Record(instance);
+            expect(clone).to.deep.equal(instance);
+            expect(clone).to.not.equal(instance);
+        });
+    });
+
+    describe('#changes()', () => {
+        it('should list changes', () => {
+            expect(instance.changes().length).to.equal(0);
+            instance.test = true;
+            expect(instance.changes().length).to.equal(1);
+            instance.test = false;
+            expect(instance.changes().length).to.equal(2);
+            instance.test = [];
+            expect(instance.changes().length).to.equal(3);
+            instance.test.push(1);
+            expect(instance.changes().length).to.equal(4);
+            instance.test.push(2);
+            instance.test.pop();
+            instance.test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            expect(instance.changes().length).to.equal(7);
+        });
+
+        it('should not list changes of a virtual property by default', () => {
+            const start = instance.changes().length;
+            instance.number = 5;
+            expect(instance.changes().length).to.equal(start);
+            expect(instance.number).to.equal(5);
+        });
+
+        it('should list changes of a virtual property if `save` is true', () => {
+            instance.saved = true;
+            expect(instance.changes(1)[0]).to.deep.equal({
+                operation: 'replace',
+                key: 'saved',
+                old: 'no',
+                new: 'yes'
+            });
+        });
+    });
+
+    describe('#revert()', () => {
+        it('should revert changes to a string property', () => {
+            const oldVal = 'John';
+            const newVal = 'Joe';
+            instance.firstName = oldVal;
+            instance.firstName = 'Joe';
+            expect(instance.firstName).to.equal(newVal);
+            expect(instance.firstName).to.not.equal(oldVal);
+            instance.revert(1);
+            expect(instance.firstName).to.equal(oldVal);
+        });
+
+        it('should revert changes to an array', () => {
+            instance.arr = [];
+            instance.revert(1);
+            expect(instance.arr).to.not.exist;
+            instance.arr = [];
+            instance.arr.push(1);
+            instance.arr[1] = 'hello';
+            instance.arr[1] = 'goodbye';
+            instance.revert(1);
+            expect(instance.arr[1]).to.equal('hello');
+            instance.arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+            instance.revert(1);
+            expect(instance.arr).to.deep.equal([1, 'hello']);
+        });
+
+        it('should revert changes to an object', () => {
+            instance.obj = {};
+            instance.revert(1);
+            expect(instance.obj).to.not.exist;
+            instance.obj = {
+                hello: 'world',
+                one: 1,
+                test: true
+            };
+            instance.obj['two'] = 2;
+            expect(instance.obj).to.deep.equal({
+                hello: 'world',
+                one: 1,
+                test: true,
+                two: 2
+            });
+            instance.obj = {tryAgain: true};
+            instance.revert(2);
+            expect(instance.obj).to.have.keys(['hello', 'one', 'test']);
+            expect(instance.obj.two).to.not.exist;
+            delete instance.obj.one;
+            expect(instance.obj.one).to.not.exist;
+
+            try {
+                delete instance.obj.one;
+            } catch (e) {
+                if (e.name !== 'TypeError') throw e;
+            }
+
+            instance.revert(1);
+            expect(instance.obj.one).to.equal(1);
+        });
+
+        it('should revert all changes', () => {
+            instance.revert();
+
+            expect(instance.changes().length).to.equal(0);
+            expect(instance['_data']).to.deep.equal(Object.assign({}, data, {
+                saved: 'no'
+            }));
+        });
+
+        it('should not throw when there are no changes to revert and `count` is not specified', () => {
+            expect(instance.revert()).to.not.throw;
+        });
+    });
+
+    describe('#save()', () => {
+        it('should call save handler and reset changes', (done) => {
+            let emitted = false;
+
+            instance.on('saved', () => {
+                emitted = true;
+            });
+            instance.save().then(() => {
+                expect(emitted).to.equal(true);
+                expect(instance.changes().length).to.equal(0);
+            }).then(done, done);
+
+            setTimeout(() => {
+                return done('Did not emit!');
+            }, 200);
+        });
+
+        it('should not commit changes if save handler is not defined', () => {
+            const i = new Record({});
+            i.test = 1;
+            expect(i.changes().length).to.equal(1);
+            return i.save().then(() => {
+                expect(i.test).to.equal(1);
+                expect(i.changes().length).to.equal(1);
+            });
+        });
+    });
+
+    describe('#toJSON()', () => {
+        it('should only return virtual property values where `toJSON` is true in definition', () => {
+            const result = instance.toJSON();
+            expect(result.saved).to.not.exist;
+        });
+    });
+});
