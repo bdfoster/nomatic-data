9ef08745d0ea0b9190a09e56477243a3390b1d6a
Brian Foster <bdfoster89@gmail.com>
2017-09-11 10:21:06 -0400

    Expand abstract Adapter and DatabaseAdapter classes

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -1,4 +1,6 @@
 import {EventEmitter} from 'nomatic-events';
+import {RecordData} from "../Record";
+import {Filter} from "../index";
 
 export interface AdapterOptions {
     name: string;
@@ -43,11 +45,17 @@ export abstract class Adapter extends EventEmitter {
      */
     public abstract dropCollection (name: string): Promise<boolean>;
 
+    public abstract get (collection: string, id: string): Promise<void | RecordData>;
+
     /**
      * Get the name of each collection already defined.
      * @returns {Promise<string[]>}
      */
     public abstract getCollectionNames (): Promise<string[]>;
+
+    public abstract findAll (collection: string, filter: Filter): Promise<RecordData[]>;
+
+    public abstract insert (collection: string, data: RecordData): Promise<void | RecordData>;
 
     /**
      * Establish a connection and/or do anything needed in order to accept queries.
@@ -57,12 +65,18 @@ export abstract class Adapter extends EventEmitter {
         return Promise.resolve(true);
     }
 
+    public abstract remove (collection: string, id: string): Promise<void | RecordData>;
+
+    public abstract replace(collection: string, id: string, data: RecordData, rev?: string): Promise<void | RecordData>;
+
     /**
      * Remove all records from the collection, but keep the collection itself and all
      * related settings.
      * @returns {Promise<boolean>}
      */
-    public abstract truncateCollection (): Promise<boolean>;
+    public abstract truncateCollection (name: string): Promise<boolean>;
+
+    public abstract update (collection: string, id: string, data: RecordData): Promise<void | RecordData>;
 }
 
 export abstract class DatabaseAdapter extends Adapter {
@@ -72,9 +86,11 @@ export abstract class DatabaseAdapter extends Adapter {
 
     public abstract createDatabase (name?: string): Promise<boolean>;
 
-    public abstract ensureCollectionExists (name?: string): Promise<boolean>;
+    public abstract ensureDatabaseExists (name?: string): Promise<boolean>;
 
-    public abstract getDatabaseNames (): Promise<boolean>;
+    public abstract dropDatabase(name: string);
+
+    public abstract getDatabaseNames (): Promise<string[]>;
 
     public abstract truncateDatabase (name?: string): Promise<boolean>;
 }
