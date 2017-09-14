13c1c9af75e8c9240949424732b82b6ee8668b3e
Brian Foster <bdfoster89@gmail.com>
2017-09-14 13:13:19 -0400

    Add log lines

--- scripts/arangodb/start.sh
+++ scripts/arangodb/start.sh
@@ -5,17 +5,20 @@ CONTINUE=1
 
 if [ ! "$(docker ps -q -f name=${NAME})" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=${NAME})" ]; then
-        # cleanup
+        echo "Removing container: ${NAME}"
         docker rm ${NAME}
     fi
 
     docker pull arangodb:latest
+    echo "Starting ArangoDB container: ${NAME}"
     docker run -d -e ARANGO_NO_AUTH=1 -p 127.0.0.1:8529:8529 --name ${NAME} arangodb:latest
 fi
 
 docker ps -f name=${NAME}
 while [ $CONTINUE -eq 1 ]; do
     sleep 1 &&
-    curl -s 127.0.0.1:8529/_api/version &&
+    echo "$(curl 127.0.0.1:8529/_api/version)" &&
     CONTINUE=0;
 done
+sleep 5
+echo "Connection to ArangoDB is live!"
