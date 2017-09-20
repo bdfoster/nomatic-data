90b452980a77d24905c95e5d930d2182aa9d54d8
Brian Foster <bdfoster89@gmail.com>
2017-09-20 06:42:17 -0400

    Add Mapper class, add tests

---
+++ src/Mapper.ts
@@ -0,0 +1,226 @@
+import {Database, DocumentCollection, Graph} from 'arangojs';
+import {EventEmitter} from 'nomatic-events';
+import {Adapter} from './adapters/index';
+import {Record, RecordData, RecordOptions, RecordValidateFunction, RecordVirtualProperties} from './Record';
+import * as merge from 'lodash.merge';
+import Query from "./Query";
+export type MapperHookFunction = (record: Record) => void;
+
+export interface MapperOptions {
+    adapter: Adapter;
+    afterInsert?: MapperHookFunction | MapperHookFunction[];
+    afterUpdate?: MapperHookFunction | MapperHookFunction[];
+    beforeInsert?: MapperHookFunction | MapperHookFunction[];
+    beforeUpdate?: MapperHookFunction | MapperHookFunction[];
+    name: string;
+    validate?: RecordValidateFunction;
+    virtuals?: RecordVirtualProperties;
+}
+
+export class Mapper extends EventEmitter {
+    static hooksList: string[] = [
+        'afterInsert',
+        'afterUpdate',
+        'beforeInsert',
+        'beforeUpdate'
+    ];
+
+    private _validate: RecordValidateFunction;
+    private _virtuals: RecordVirtualProperties;
+
+    public readonly adapter: Adapter;
+    public name: string;
+    public readonly collection: string;
+
+    constructor(options: MapperOptions) {
+        super();
+        this.adapter = options.adapter;
+        this.name = options.name;
+        this.collection = options.name;
+        this._validate = options.validate;
+        this._virtuals = options.virtuals || {};
+
+        for (const namespace of Mapper.hooksList) {
+            if (options[namespace]) {
+                if (options[namespace] instanceof Array) {
+                    for (const fn of options[namespace]) {
+                        this.on(namespace, fn);
+                    }
+                } else {
+                    this.on(namespace, options[namespace]);
+                }
+            }
+        }
+    }
+
+    public createRecord(data: RecordData = {}): Record {
+        const options: RecordOptions = {
+            validate: this._validate,
+            save: (record) => this.save(record),
+            virtuals: this._virtuals
+        };
+
+        return new Record(options, data);
+    }
+
+    public async get(id: string): Promise<Record> {
+        const response = await this.adapter.get(this.collection, id);
+        return this.createRecord(response);
+    }
+
+    public find(): Query {
+        return new Query((query) => {
+           return this.findAll(query);
+        });
+    }
+
+    public async findAll(query: Query | object): Promise<Record[]> {
+        let q;
+
+        if (!(query instanceof Query)) {
+            q = new Query(null, query);
+        } else {
+            q = query;
+        }
+
+        const results = [];
+        const response = await this.adapter.findAll(this.collection, q);
+
+        for (const i in response) {
+            results.push(this.createRecord(response[i]));
+        }
+
+        return results;
+    }
+
+    public async update(data: Record | RecordData, validate: boolean = true): Promise<Record> {
+        let record;
+
+        if (!(data instanceof Record)) {
+            const old = await this.get(data.id);
+            merge(old, data);
+            record = data;
+        } else {
+            record = data;
+        }
+
+        if (validate) {
+            try {
+                await record.validate();
+            } catch (error) {
+                throw error;
+            }
+        }
+
+        this.emit('beforeUpdate', record);
+
+        const result = await this.adapter.update(this.collection, record.id, record.serialize());
+        record.commit(result);
+        this.emit('afterUpdate', record);
+        return record;
+    }
+
+    public async save(record: Record, validate: boolean = true): Promise<Record> {
+        if (record.id && record.rev) {
+            if (record.changes().length === 0) {
+                return this.get(record.id).then((response) => {
+                    if (response.rev === record.rev) {
+                        return record;
+                    }
+
+                    return this.update(record, validate);
+                });
+            }
+
+            return this.update(record, validate);
+        }
+
+        return this.insert(record, validate);
+    }
+
+    public async open() {
+        try {
+            await this.adapter.ensureCollectionExists(this.collection);
+            return this;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    public async remove (data: RecordData | string): Promise<Record> {
+        let response;
+
+        if (typeof data === 'string') {
+            response = await this.adapter.remove(this.collection, data);
+        } else {
+            response = await this.adapter.remove(this.collection, data.id);
+        }
+
+        return response;
+    }
+
+    public async replace (id: string, data: RecordData, validate: boolean = true, rev: string = null): Promise<Record> {
+        const record = this.createRecord(Object.assign({}, data));
+
+        if (validate) {
+            try {
+                await record.validate();
+            } catch (error) {
+                throw error;
+            }
+        }
+
+        this.emit('beforeUpdate', record);
+
+        return await this.adapter.replace(this.collection, id, record.serialize(), rev).then((data) => {
+            record.commit(data);
+            this.emit('afterUpdate', record);
+            return record;
+        });
+    }
+
+    public async insert(data: Record | RecordData, validate: boolean = true): Promise<Record> {
+        let record;
+
+        if (!(data instanceof Record)) {
+            record = this.createRecord(data);
+        } else {
+            record = data;
+        }
+
+        if (validate) {
+            try {
+                await data.validate();
+            } catch (error) {
+                throw error;
+            }
+        }
+
+        this.emit('beforeInsert', data);
+
+        return await this.adapter.insert(this.collection, record.serialize()).then((result) => {
+            record.init(result);
+            this.emit('afterInsert', record);
+            return record;
+        });
+    }
+
+    public async insertAll(records: (Record | RecordData)[], validate: boolean = true): Promise<Record[]> {
+        const results = [];
+        for (const record of records) {
+            try {
+                results.push(await this.insert(record, validate));
+            } catch (error) {
+                throw error;
+            }
+        }
+
+        return results;
+    }
+
+    public truncate () {
+        return this.adapter.truncateCollection(this.collection);
+    }
+}
+
+export default Mapper;
---
+++ test/Mapper.test.ts
@@ -0,0 +1,208 @@
+import 'mocha';
+import {expect} from 'chai';
+import Mapper from '../src/Mapper';
+import Record from '../src/Record';
+import * as util from 'util';
+import NotFoundError from '../src/errors/NotFoundError';
+import ArangoDBAdapter from '../src/adapters/ArangoDBAdapter';
+
+process.on('unhandledRejection', (reason) => {
+    console.error(reason);
+    process.exit(1);
+});
+
+describe('Mapper', () => {
+    const records = [];
+    const data = [
+        {
+            firstName: 'John',
+            lastName: 'Doe'
+
+        },
+
+        {
+            id: '12345',
+            firstName: 'Jane',
+            lastName: 'Doe',
+            birthDate: '1989-01-01'
+        }
+
+    ];
+
+    let people: Mapper;
+
+    before(function (done) {
+        let config = require('./fixtures/config/' + process.env.NODE_ENV + '.json')['arangodb'];
+        people = new Mapper({
+            adapter: new ArangoDBAdapter(config),
+            name: 'person',
+        });
+        people.open().then(() => {
+            return people.truncate().then(() => {
+                records.push(people.createRecord(data[0]));
+                records.push(people.createRecord(data[1]));
+                return done();
+            });
+        }).catch(done);
+    });
+
+    describe('#create()', () => {
+        it('should create a new Record', () => {
+            expect(records[0]).to.be.instanceOf(Record);
+            expect(records[0]['_data']).to.deep.equal(data[0]);
+        });
+
+        it('should create a new Record when specifying `id`', () => {
+            expect(records[1].id).to.equal(data[1]['id']);
+            expect(records[1].toJSON()).to.deep.equal(data[1]);
+            expect(records[1].rev).to.not.exist;
+        });
+    });
+
+    describe('#save()', () => {
+        it('should save the record to the database collection when `id` is not specified', (done) => {
+            records[0].save().then(() => {
+                expect(records[0].id).to.exist;
+                expect(records[0].rev).to.exist;
+                data[0] = records[0].toJSON();
+            }).then(done, done);
+        });
+
+        it('should save the record to the database collection when `id` is specified', (done) => {
+            records[1].save().then(() => {
+                expect(records[1].id).to.equal(data[1]['id']);
+                expect(records[1].rev).to.exist;
+                data[1] = records[1].toJSON();
+            }).then(done, done);
+        });
+
+        it('should update the record to the database collection', (done) => {
+            records[0].birthDate = '2000-12-31';
+            records[0].save().then(() => {
+                expect(data[0]['rev']).to.not.equal(records[0].rev);
+                expect(records[0].birthDate).to.equal('2000-12-31');
+                data[0] = records[0].toJSON();
+            }).then(done, done);
+        });
+
+        it('should bypass database operations when no changes are made', (done) => {
+            expect(records[0].changes().length).to.equal(0);
+            records[0].save().then(() => {
+                expect(data[0]['rev']).to.equal(records[0].rev);
+            }).then(done, done);
+        });
+
+        it('should throw if trying to use an id that already exists', (done) => {
+            const d = {
+                id: records[0].id,
+                firstName: 'Jane',
+                lastName: 'Doe',
+                birthDate: '1989-01-01'
+            };
+
+            const record = people.createRecord(d);
+
+            record.save().then(() => {
+                return done('Did not throw!');
+            }).catch((e) => {
+                if (e.name === 'AlreadyExistsError') {
+                    return done();
+                }
+
+                return done(e);
+            });
+        });
+    });
+
+    describe('#findAll', () => {
+        it('should find the saved Record', (done) => {
+            people.findAll({
+                $where: {
+                    id: records[0].id
+                }
+            }).then(results => {
+                expect(results[0].serialize()).to.deep.equal(records[0].serialize());
+                return done();
+            }).catch(done);
+        });
+    });
+
+    describe('#get()', () => {
+        it('should get the saved Record', (done) => {
+            people.get(data[0]['id']).then((record) => {
+                expect(record).to.exist;
+                expect(record).to.be.instanceOf(Record);
+                expect(record.id).to.equal(records[0].id);
+                return done();
+            }).catch(done);
+        });
+
+        it('should throw when specifying a non-existent Record', (done) => {
+            people.get('000000').then(() => {
+                return done('Did not throw!');
+            }).catch((e) => {
+                if (e.name === 'NotFoundError') {
+                    return done();
+                }
+
+                return done(e);
+            });
+        });
+    });
+
+    describe('#update()', () => {
+        it('should throw if revision is invalid', (done) => {
+            const rev = '_Vaactly---';
+            records[0].rev = rev;
+            records[0]['middleName'] = 'Richard';
+            people.update(records[0]).then(() => {
+                return done('Did not throw!');
+            }).catch(e => {
+                if (e.name === 'AssertionError' && e.message.startsWith('Assertion Error: expected `rev` to equal `' + rev)) {
+                    return done();
+                }
+                return done(e);
+            });
+        });
+    });
+
+    describe('#remove()', () => {
+        it('should delete the saved Record', (done) => {
+            people.remove(data[0]).then(() => {
+                return people.get(data[0]['id']).then(() => {
+                    return done('Did not throw!')
+                }).catch((e) => {
+                    if (e.name === 'NotFoundError') {
+                        return done();
+                    }
+                    return done(e);
+                });
+            }).catch(e => { console.log(util.inspect(e, true, Infinity)); return done(e); });
+        });
+
+        it('should delete the saved Record when only passing `id`', (done) => {
+            people.remove(data[1]['id']).then(() => {
+                return people.get(data[1]['id']).then(() => {
+                    return done('Did not throw!')
+                }).catch((e) => {
+                    if (e.name === 'NotFoundError') {
+                        return done();
+                    }
+                    return done(e);
+                });
+            }).catch(e => { console.log(util.inspect(e, true, Infinity)); return done(e); });
+        });
+
+        it('should throw when re-deleting saved Record', (done) => {
+            people.remove(data[0]['id']).then(() => {
+                return done('Did not throw!');
+            }).catch((e) => {
+                if (e instanceof NotFoundError) {
+                    return done();
+                }
+                console.error(e);
+                return done(e);
+            });
+        });
+    });
+});
